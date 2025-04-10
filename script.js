2e0ef47c71ab5ef7aa680799b839b016f7132e7d
Thailan <thailangm@gmail.com>
2025-04-10 18:11:52 +0300

    Feat: makes game interactable through the gameboard

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const Gameboard = (function() {
       return false;
     } else {
       gameboard[positionY][positionX] = player;
+
       return true;
     }
   }
@@ -18,6 +19,77 @@ const Gameboard = (function() {
 
   return { addToBoard, getBoard };
 })();
+
+function updateBoard(boardPosition, player) {
+  const square = document.createElement("p");
+  const board = document.querySelector("#gameboard_container");
+  switch (boardPosition) {
+    case 0:
+      Gameboard.addToBoard(0, 0, player);
+      square.innerText = player.symbol;
+      if (!(board.children[boardPosition].innerText == 'X') && !(board.children[boardPosition].innerText == 'O')) {
+        board.replaceChild(square, board.childNodes[boardPosition]);
+      }
+      break;
+    case 1:
+      Gameboard.addToBoard(0, 1, player);
+      square.innerText = player.symbol;
+      if (!(board.children[boardPosition].innerText == 'X') && !(board.children[boardPosition].innerText == 'O')) {
+        board.replaceChild(square, board.childNodes[boardPosition]);
+      }
+      break;
+    case 2:
+      Gameboard.addToBoard(0, 2, player);
+      square.innerText = player.symbol;
+      if (!(board.children[boardPosition].innerText == 'X') && !(board.children[boardPosition].innerText == 'O')) {
+        board.replaceChild(square, board.childNodes[boardPosition]);
+      }
+      break;
+    case 3:
+      Gameboard.addToBoard(1, 0, player);
+      square.innerText = player.symbol;
+      if (!(board.children[boardPosition].innerText == 'X') && !(board.children[boardPosition].innerText == 'O')) {
+        board.replaceChild(square, board.childNodes[boardPosition]);
+      }
+      break;
+
+    case 4:
+      Gameboard.addToBoard(1, 1, player);
+      square.innerText = player.symbol;
+      if (!(board.children[boardPosition].innerText == 'X') && !(board.children[boardPosition].innerText == 'O')) {
+        board.replaceChild(square, board.childNodes[boardPosition]);
+      }
+      break;
+    case 5:
+      Gameboard.addToBoard(1, 2, player);
+      square.innerText = player.symbol;
+      if (!(board.children[boardPosition].innerText == 'X') && !(board.children[boardPosition].innerText == 'O')) {
+        board.replaceChild(square, board.childNodes[boardPosition]);
+      }
+      break;
+    case 6:
+      Gameboard.addToBoard(2, 0, player);
+      square.innerText = player.symbol;
+      if (!(board.children[boardPosition].innerText == 'X') && !(board.children[boardPosition].innerText == 'O')) {
+        board.replaceChild(square, board.childNodes[boardPosition]);
+      }
+      break;
+    case 7:
+      Gameboard.addToBoard(2, 1, player);
+      square.innerText = player.symbol;
+      if (!(board.children[boardPosition].innerText == 'X') && !(board.children[boardPosition].innerText == 'O')) {
+        board.replaceChild(square, board.childNodes[boardPosition]);
+      }
+      break;
+    case 8:
+      Gameboard.addToBoard(2, 2, player);
+      square.innerText = player.symbol;
+      if (!(board.children[boardPosition].innerText == 'X') && !(board.children[boardPosition].innerText == 'O')) {
+        board.replaceChild(square, board.childNodes[boardPosition]);
+      }
+      break;
+  }
+}
 
 function createUser(name, symbol) {
   let userScore = 0;
@@ -33,11 +105,11 @@ function checkMatchWinner() {
 
   for (let i = 0; i < 3; i++) {
     if (Gameboard.getBoard()[i][0].symbol == Gameboard.getBoard()[i][1].symbol && Gameboard.getBoard()[i][1].symbol == Gameboard.getBoard()[i][2].symbol) {
-      console.log(`${Gameboard.getBoard()[i][0].name} is the winner!`);
+      header.innerText = `${Gameboard.getBoard()[i][0].name} is the winner!`;
       Gameboard.getBoard()[i][0].setWin();
       return true;
     } else if (Gameboard.getBoard()[0][i].symbol == Gameboard.getBoard()[1][i].symbol && Gameboard.getBoard()[1][i].symbol == Gameboard.getBoard()[2][i].symbol) {
-      console.log(`${Gameboard.getBoard()[0][i].name} is the winner!`);
+      header.innerText = `${Gameboard.getBoard()[0][i].name} is the winner!`;
       Gameboard.getBoard()[0][i].setWin();
       return true;
     }
@@ -45,13 +117,13 @@ function checkMatchWinner() {
   }
 
   if (Gameboard.getBoard()[0][0].symbol == Gameboard.getBoard()[1][1].symbol && Gameboard.getBoard()[1][1].symbol == Gameboard.getBoard()[2][2].symbol) {
-    console.log(`${Gameboard.getBoard()[0][0].name} is the winner!`);
+    header.innerText = `${Gameboard.getBoard()[0][0].name} is the winner!`;
     Gameboard.getBoard()[0][0].setWin();
     return true;
   }
 
   if (Gameboard.getBoard()[0][2].symbol == Gameboard.getBoard()[1][1].symbol && Gameboard.getBoard()[1][1].symbol == Gameboard.getBoard()[2][0].symbol) {
-    console.log(`${Gameboard.getBoard()[0][2].name} is the winner!`);
+    header.innerText = `${Gameboard.getBoard()[0][2].name} is the winner!`;
     Gameboard.getBoard()[0][2].setWin();
     return true;
   }
@@ -59,39 +131,32 @@ function checkMatchWinner() {
   return false;
 }
 
+let outterCounter = 0;
 
 function gameControl(
   playerOne,
   playerTwo = createUser("CPU", playerOne.symbol == "X" ? "O" : "X"),
+  e
 ) {
-  let positionX;
-  let positionY;
 
-  let outterCounter = 0;
-
-  while (!(playerOne.getWin()) && !(playerTwo.getWin())) {
-
-    if (outterCounter % 2 == 0) {
-      positionX = Number(prompt(`${playerOne.name}, pick your X position`));
-      positionY = Number(prompt(`${playerOne.name}, pick your Y position`));
-      Gameboard.addToBoard(positionY, positionX, playerOne);
-    } else {
-      positionX = Number(prompt(`${playerTwo.name}, pick your X position`));
-      positionY = Number(prompt(`${playerTwo.name}, pick your Y position`));
-      Gameboard.addToBoard(positionY, positionX, playerTwo);
-    }
-
-    outterCounter += 1;
-
-
-    if (outterCounter >= 4) {
-      checkMatchWinner();
-    } else if (outterCounter == 8 && !(checkMatchWinner())) {
-      console.log("It's a tie!");
-      return;
-    }
-
+  if (outterCounter % 2 == 0 && !(checkMatchWinner())) {
+    updateBoard(Number(e.target.innerText), playerOne);
+    header.innerText = `${playerTwo.name}, it's your turn!`;
+  } else if (outterCounter % 2 != 0 && !(checkMatchWinner())) {
+    updateBoard(Number(e.target.innerText), playerTwo);
+    header.innerText = `${playerOne.name}, it's your turn!`;
   }
+
+  outterCounter += 1;
+
+
+  if (outterCounter >= 4) {
+    checkMatchWinner();
+  } else if (outterCounter == 8 && !(checkMatchWinner())) {
+    header.innerText = "It's a tie!";
+    return;
+  }
+
 
 }
 
@@ -100,13 +165,26 @@ let secondCounter = 0;
 
 for (let i = 0; i < 3; i++) {
   for (let j = 0; j < 3; j++) {
-    const basePlayer = createUser("Void", `A${secondCounter}`);
+    const basePlayer = createUser("Void", `${secondCounter}`);
     secondCounter++;
     Gameboard.addToBoard(i, j, basePlayer);
+    const board = document.querySelector("#gameboard_container");
+    const square = document.createElement("p");
+    square.classList.add("non-player");
+    square.innerText = basePlayer.symbol;
+    board.appendChild(square);
   }
 }
 
 let firstPlayer = createUser("Thailan", "X");
 let secondPlayer = createUser("Blanda", "O");
-gameControl(firstPlayer, secondPlayer);
 
+const board = document.querySelector("#gameboard_container");
+const header = document.querySelector("header h1");
+
+header.addEventListener("click", (e1) => {
+  header.innerText = `${firstPlayer.name}, it's your turn!`;
+  board.addEventListener("click", (e2) => {
+    gameControl(firstPlayer, secondPlayer, e2);
+  })
+})
